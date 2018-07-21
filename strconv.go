23f88dd4ea4151ad53ef8de29e8d471eb1a9bda1
John <admin@iios.net>
2018-07-21 17:26:16 +0800

    use glide add utf8,gbk and bi5 translate

--- strconv.go
+++ strconv.go
@@ -1,5 +1,13 @@
 package mantis
 
+import (
+	"bytes"
+	"io/ioutil"
+	"golang.org/x/text/encoding/traditionalchinese"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
 //全角转换半角s
 func DBCtoSBC(s string) string {
 	res := ""
@@ -17,4 +25,37 @@ func DBCtoSBC(s string) string {
 		}
 	}
 	return res
+}
+
+//convert GBK to UTF-8
+func DecodeGBK(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+//convert BIG5 to UTF-8
+func DecodeBig5(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, traditionalchinese.Big5.NewDecoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+//convert UTF-8 to BIG5
+func EncodeBig5(s []byte) ([]byte, error) {
+	I := bytes.NewReader(s)
+	O := transform.NewReader(I, traditionalchinese.Big5.NewEncoder())
+	d, e := ioutil.ReadAll(O)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
 }
