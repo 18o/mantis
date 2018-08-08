999dfa8ac46f3024852e4bc8466ed7582050a7e3
John <admin@iios.net>
2018-08-08 21:49:38 +0800

    增加MD5 Hash 增加简体中文转台湾繁体

--- strconv.go
+++ strconv.go
@@ -6,6 +6,12 @@ import (
 	"golang.org/x/text/encoding/traditionalchinese"
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
+	"os/exec"
+	"os"
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"strings"
 )
 
 //全角转换半角s
@@ -58,4 +64,33 @@ func EncodeBig5(s []byte) ([]byte, error) {
 		return nil, e
 	}
 	return d, nil
+}
+// 简体中文转台湾繁体中文，需要安装opencc
+func Trans2TW(s string) string {
+	filepath := "/tmp/" + GetMD5Hash(s)
+	writeToFile(filepath, s)
+	cmd := exec.Command("opencc", "-c", "s2tw.json", "-i", filepath, "-o", filepath)
+	cmd.Run()
+	b, _ := ioutil.ReadFile(filepath)
+	os.Remove(filepath)
+	return string(b)
+}
+
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func writeToFile(filepath, s string) error {
+	fo, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer fo.Close()
+	_, err = io.Copy(fo, strings.NewReader(s))
+	if err != nil {
+		return nil
+	}
+	return nil
 }
